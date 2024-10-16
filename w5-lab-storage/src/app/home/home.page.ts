a361b98938dd07b09cc6c7168fea81dd0bb9073b
Oisin Hearne <48627086+Oisin-Hearne@users.noreply.github.com>
2024-10-16 20:08:00 +0000

    Completed home.page.ts, created storage service.

--- w5-lab-storage/src/app/home/home.page.ts
+++ w5-lab-storage/src/app/home/home.page.ts
@@ -1,13 +1,94 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Storage } from '@ionic/storage-angular';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [ FormsModule, IonicModule],
 })
 export class HomePage {
-  constructor() {}
+  key: string = ""
+  value: string = ""
+  output: string = ""
+  keys: string[] = [""]
+  pairs: string[] = []
+
+  constructor(private storage: Storage) {
+    storage.create();
+  }
+
+  async setItem() {
+    try {
+      await this.storage.set(this.key, this.value);
+      console.log(this.key)
+      this.output = `Set ${this.key}: ${this.value}`;
+    } catch (error) {
+      console.error("Error setting item", error);
+      this.output = `Error setting item: ${error}`;
+    }
+  }
+
+  async getItem() {
+    try {
+      const value = await this.storage.get(this.key);
+      this.output = `Get ${this.key}: ${this.value}`
+    } catch (error) {
+      console.error("Error getting item", error)
+      this.output = `Error getting item: ${error}`
+    }
+  }
+
+  async removeItem() {
+    try{
+      this.output = `Deleting ${this.key}...`
+      await this.storage.remove(this.key);
+    } catch (error) {
+      console.error("Error deleting item", error)
+      this.output = `Error deleting item: ${error}`
+    }
+  }
+
+  async clearItems() {
+    try{
+      this.output = `Clearing all keys.`
+      await this.storage.clear();
+    } catch (error) {
+      console.error("Error deleting items", error)
+      this.output = `Error deleting items: ${error}`
+    }
+  }
+
+  async getKeys() {
+    try{
+      this.keys = await this.storage.keys()
+      this.output = `Keys: ${this.keys}`
+    } catch (error) {
+      console.error("Error getting keys", error)
+      this.output = `Error getting keys: ${error}`
+    }
+  }
+
+  async numItems() {
+    try{
+      this.output = `Items: ${await this.storage.length()}...`
+    } catch (error) {
+      console.error("Error getting items", error)
+      this.output = `Error getting item: ${error}`
+    }
+  }
+
+  async forKeys() {
+    try{
+      await this.storage.forEach((k, v) => {this.pairs.push(k, v)})
+      this.output = `Items: ${this.pairs}`
+      this.pairs = []
+    } catch (error) {
+      console.error("Error getting items", error)
+      this.output = `Error getting items: ${error}`
+    }
+  }
 }
